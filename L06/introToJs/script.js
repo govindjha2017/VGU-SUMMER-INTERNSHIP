a4e1502fcfd6802c44562a938e39e1c51f2daa01
govind <govindkummarjha2017@gmail.com>
2026-06-23 16:09:38 +0530

    23 jume

---
+++ L06/introToJs/script.js
@@ -0,0 +1,43 @@
+console.log("Hello from my first js file");
+
+// var let const 
+
+// var x = 40;
+// console.log(x);
+// var x;
+// x=11;
+// var x=13
+// console.log(x)
+
+// let a=15;
+// console.log(a);
+
+// // let a;  can't do this
+// // let a=11;  can't do this
+
+// a=21;
+// console.log(a);
+
+
+// const temp=40;
+
+// console.log(temp);
+
+// // const temp;  can't do this
+
+// temp = 11;
+
+
+{
+    var a=11;
+    let b=12;
+    const c=13;
+    console.log(a,b,c)
+}
+
+console.log(a);
+
+// console.log(b);
+// console.log(c);
+
+ 
