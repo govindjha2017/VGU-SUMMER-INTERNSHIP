ebffe4f1c2f368a9649839c29ca13373bf7d1cef
govind <govindkummarjha2017@gmail.com>
2026-06-24 12:12:20 +0530

    24 june first phase

---
+++ L07/array/script.js
@@ -0,0 +1,63 @@
+// console.log("Hello ji!");
+
+// let arr = [33,45,66,"hello",11,95,76];
+// console.log("before",arr)
+
+// arr.push(13);
+// arr.push(11);
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+// arr.unshift("start");
+// arr.unshift(12);
+
+// arr.shift()
+// arr.shift()
+
+// arr.splice(2,1);
+
+// arr.splice(2,3);
+
+// let arr = [33,45,66,"hello",11,95,76];
+// arr.splice(3,1,7,17);
+
+// let x = arr.slice(2,5);
+// console.log(x);
+
+
+
+
+// console.log("after",arr)
+
+
+// let arr1 = [11,76,43];
+
+// let arr2= [13,74,33,9];
+
+// let a= arr1.concat(arr2);
+// console.log(a);
+
+// let a= arr2.concat(arr1);
+// console.log(a);
+
+
+let arr = [33,45,66,"hello",11,95,76];
+
+
+// console.log(arr[2])
+
+// normal loop
+
+// for(let i=0;i<arr.length;i++){
+//     // console.log("abc");
+
+//     console.log(arr[i]);
+// }
+
+// for of loop
+// let arr = [33,45,66,"hello",11,95,76];
+for(let item of arr){
+    console.log(item);
+}
---
+++ L07/string/script.js
@@ -0,0 +1,20 @@
+let str = "This is String and we are learning JS";
+console.log(str);
+
+// let str1 = 'Hello ji kaise ho ??';
+// console.log(str1);
+
+
+// let str2 = `main theek hu!`;
+// console.log(str2);
+
+// let x = 'A';
+// let y="B";
+
+// let p = str.replace("is","IS");
+// let p = str.replaceAll("is","ABCD");
+
+// console.log(p)
+
+
+// split
