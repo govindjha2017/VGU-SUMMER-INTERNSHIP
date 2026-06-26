fade72f29cc19c8999edef7a60c55a4057fb30d1
govind <govindkummarjha2017@gmail.com>
2026-06-26 11:57:07 +0530

    26 june 1st phase

---
+++ L08/function/script.js
@@ -0,0 +1,68 @@
+
+// function abc(){
+//     console.log("inside abc function");
+// }
+
+// abc()
+
+
+// function def(){
+//     let a=11;
+//     return a;
+// }
+
+// let x = def();
+// console.log(x);
+
+
+// function fun(){
+//     let p="Hello";
+//     console.log("inside fun function!")
+//     return p;
+// }
+
+// let y = fun;
+
+// console.log(y);
+
+// // fun()
+// y();
+
+
+// let y = function fun(){
+//     let p="Hello";
+//     console.log("inside fun function!")
+//     return p;
+// }
+
+// console.log(y);
+
+// console.log(fun);
+
+
+// anonymous function
+
+// let y = function (){
+//     let p="Hello";
+//     console.log("inside fun function!")
+//     return p;
+// }
+
+// y();
+
+// arrow function
+
+// let y =  ()=>{
+//     let p="Hello";
+//     console.log("inside fun function!")
+//     return p;
+// }
+
+// y();
+
+
+
+
+let add = (a,b)=>{
+    return a+b;
+}
---
+++ L08/temp/script.js
@@ -0,0 +1,29 @@
+
+// console.log(x);
+// var x=20;
+// console.log(x);
+
+// console.log(x);
+// let x=20;
+// console.log(x);
+
+// console.log(x);
+// const x=20;
+// console.log(x);
+
+// console.log(p);
+
+conaole.log(x);
+var x=20;
+conaole.log(x);
+
+function abc(){
+    let a=10;
+    console.log(a);
+    console.log(x);
+}
+console.log(p);
+let p=20
+
+console.log(a);
+console.log("END");
