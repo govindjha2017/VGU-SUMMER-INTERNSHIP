7ec66a856ffa103bf5b51ef9fd0b7c9a00186d3f
govind <govindkummarjha2017@gmail.com>
2026-07-13 12:13:29 +0530

    13 july 1st phase

---
+++ L20/ProductApp/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const products = require("./data/products");
+
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/products",(req,res)=>{
+    res.render("product",{products})
+})
+
+app.get("/product/new",(req,res)=>{
+    res.render("new")
+})
+
+app.post("/products",(req,res)=>{
+    const {name,image,price,desc} = req.body;
+    let id;
+    if(products.length>0){
+        id=products[products.length-1].id+1
+    }else{
+        id=1;
+    }
+    products.push({id,name,image,price,desc});
+    res.redirect("/products")
+})
+
+app.get("/products/:id",(req,res)=>{
+    const {id} = req.params;
+    const product = products.find((item)=> item.id==id);
+
+    res.render("show",{product})
+})
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log("Server run at port",PORT);
+})
