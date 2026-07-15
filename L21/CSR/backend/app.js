4fc161445c92c43ad39818eba6dd8414023a633a
govind <govindkummarjha2017@gmail.com>
2026-07-15 11:57:16 +0530

    15 july 1st phase

--- L21/CSR/backend/app.js
+++ L21/CSR/backend/app.js
@@ -5,12 +5,20 @@ const cors = require("cors");
 app.use(cors());
 
 const todos = ["Game","Dance","music","cricket"];
+app.use(express.json())
 
 app.get("/todos",(req,res)=>{
     res.json({todos});
 })
 
-///
+
+app.post("/todos",(req,res)=>{
+     
+    let data = req.body.todo;
+    todos.push(data);
+    
+    res.json({msg:"Todo created succesfully"})
+})
 
 const PORT = 3000;
 app.listen(PORT,()=>{
--- L21/CSR/frontend/index.js
+++ L21/CSR/frontend/index.js
@@ -1,16 +1,38 @@
 const APILINK = "http://localhost:3000/todos";
 const box = document.querySelector(".box");
-fetch(APILINK)
+
+function displayTodos(){
+    box.innerHTML="";
+    fetch(APILINK)
     .then((res)=>{
         console.log(res);
         return res.json()
     })
     .then((data)=>{
         console.log(data.todos);
-
         for(let item of data.todos){
             const para = document.createElement("p");
             para.innerText=item;
             box.appendChild(para);
         }
     })
+}
+
+displayTodos();
+
+const btn = document.getElementById("btn");
+const inp = document.getElementById("inp");
+
+btn.addEventListener("click",async (e)=>{
+   let res = await fetch("http://localhost:3000/todos",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json",
+        },
+        body: JSON.stringify({
+            todo:inp.value
+        })
+    });
+    inp.value="";
+    displayTodos();
+})
